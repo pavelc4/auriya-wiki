8d8db2a62d839cdca4e57b37c898c1ff38ad926e
Pavelc4 <pavelc4@users.noreply.github.com>
2026-08-18 20:37:11 +0700

    fix: set baseUrl to /auriya-wiki/

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   title: 'Auriya Wiki',
   tagline: 'Technical documentation for Auriya',
   url: 'https://auriya-wiki.pages.dev',
-  baseUrl: '/',
+  baseUrl: '/auriya-wiki/',
   organizationName: 'pavelc4',
   projectName: 'auriya',
   onBrokenLinks: 'throw',
